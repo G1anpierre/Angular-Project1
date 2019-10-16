4d81f5cff102dfb5e52072fe616406c2fbab15e0
Gianpierre Fernandez <Gianpi@Gianpierre-MacBook-Pro.local>
2019-10-16 10:14:52 +0200

    add recipe model and add some content to the recipes components

--- prj-start/src/app/recipes/recipes-list/recipes-list.component.ts
+++ prj-start/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
+  ];
 
   constructor() { }
 
